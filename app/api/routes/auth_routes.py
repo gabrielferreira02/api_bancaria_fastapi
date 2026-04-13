27018287a4ee03a3350c72a5d0c8b6562392f30d
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2026-04-13 16:28:23 -0300

    refacttor: change schema response in register route

--- app/api/routes/auth_routes.py
+++ app/api/routes/auth_routes.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends
-from app.schemas.auth_schemas import RegisterSchema, RegisterResponseSchema
+from app.schemas.auth_schemas import RegisterSchema
+from app.schemas.user_schemas import UserResponseSchema
 from app.services.auth_service import AuthService
 from sqlalchemy.orm import Session
 from app.api.deps import get_session
 
 auth_router = APIRouter(prefix="/auth", tags=["Authentication"])
 
-@auth_router.post("/register", response_model=RegisterResponseSchema, status_code=201)
+@auth_router.post("/register", response_model=UserResponseSchema, status_code=201)
 async def register(body: RegisterSchema, session: Session = Depends(get_session)):
     return AuthService.register(body, session)
