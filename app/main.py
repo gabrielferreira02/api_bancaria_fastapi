73559e795cd08de97f61e80422d3bbb3d6aba492
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2026-04-13 16:26:27 -0300

    feat: register routers

--- app/main.py
+++ app/main.py
@@ -3,5 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 from app.api.routes.auth_routes import auth_router
+from app.api.routes.transaction_routes import transaction_router
+from app.api.routes.account_routes import account_router
 
 app.include_router(auth_router)
+app.include_router(transaction_router)
+app.include_router(account_router)
